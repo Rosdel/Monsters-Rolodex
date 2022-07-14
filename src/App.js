2759522e24a6b0d4c77a338af2c9508baa2a4991
Rosdell Ouma <abonyorosdell@gmail.com>
2022-07-14 16:02:21 +0300

    .

--- src/App.js
+++ src/App.js
@@ -7,19 +7,48 @@ class App extends Component {
     super()
     this.state = {
       monsters: [],
+      searchField: "",
     }
+    console.log("constructor")
   }
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    console.log("componentDidMount")
+    fetch("http://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState()=> {
-        return { monsters: users },
-      })
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users }
+          },
+          () => {
+            console.log(this.state)
+          }
+        )
+      )
+  }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+
+    this.setState(() => {
+      return { searchField }
+    })
   }
   render() {
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+    console.log("render")
+    const filteredSearchedMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-inbox"
+          type="search"
+          placeholder="search monster"
+          onChange={onSearchChange}
+        />
+        {filteredSearchedMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
